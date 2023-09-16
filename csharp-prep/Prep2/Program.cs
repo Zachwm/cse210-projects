a0163dad77a5d67f704e299f2142410355be16e8
Zachwm <zachwm@byui.edu>
2023-09-16 16:13:57 -0600

    Prep 1-2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string gradeString = Console.ReadLine();
+        int gradeInt = int.Parse(gradeString);
+
+        string gradeLetter;
+
+        if (gradeInt >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {gradeLetter}");
     }
 }
