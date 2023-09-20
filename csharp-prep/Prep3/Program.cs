642fa70ba33f5ccf5987ee992616f036b485afab
Zachwm <zachwm@byui.edu>
2023-09-20 11:05:13 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        bool done = false;
+
+        Console.Write("What is the magic number? ");
+        string answerString = Console.ReadLine();
+        int answerInt = int.Parse(answerString);
+        
+        while (!done)
+        {
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guessInt = int.Parse(guessString);
+
+        if (guessInt == answerInt)
+        {
+            Console.Write("You guessed it!");
+            done = true;
+        }
+        else if (guessInt < answerInt)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guessInt > answerInt)
+        {
+            Console.WriteLine("Lower");
+        }
+        }
     }
 }
