4e5b6866f63c4ee4d132c13adecb28d8df9df384
Zachwm <zachwm@byui.edu>
2023-09-23 18:17:12 -0600

    prep 5 working

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,7 +1,7 @@
-// 
+// Name and favorite number
 // CSE 210
 // Zach McLaughlin
-// 
+// takes the name and favorite number of the user and dispays their name back along with the square of their favorite number
 
 using System;
 
@@ -9,42 +9,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        void zmMain()
-        {
-            zmDisplayWelcome();
-            string zmName = zmPromptUserName();
-            int zmNumber = zmPromptUserNumber();
-            double zmSquaredNumber = SquareNumber(int zmNumber);
-            DisplayResult(string zmName, double zmSquaredNumber);
-
-        }
-        void zmDisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the Program!");
-        }
-        string zmPromptUserName()
-        {
-            Console.Write("Please enter username: ");
-            string zmUsername = Console.ReadLine();
-            return zmUsername;
-        }
-        int zmPromptUserNumber()
-        {
-            Console.Write("Please enter your favorite number: ");
-            string zmNumString = Console.ReadLine();
-            int zmNumInt = int.Parse(zmNumString);
-            return zmNumInt;
-        }
-        double SquareNumber(zmNumber)
-        {
-            double zmNumSquared = zmNumber * zmNumber;
-            return zmNumSquared;
-        }
-        void DisplayResult(zmName,  zmSquaredNumber);
-        {
-            Console.WriteLine($"{zmName}, the square of your number is {zmSquaredNumber}");
-        }
 
         zmMain();
+    }
+
+    static void zmMain()
+    {
+        zmDisplayWelcome();
+        string zmName = zmPromptUserName();
+        int zmNumber = zmPromptUserNumber();
+        double zmSquaredNumber = SquareNumber(zmNumber);
+        DisplayResult(zmName, zmSquaredNumber);
+    }
+
+    static void zmDisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string zmPromptUserName()
+    {
+        Console.Write("Please enter username: ");
+        string zmUsername = Console.ReadLine();
+        return zmUsername;
+    }
+    static int zmPromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string zmNumString = Console.ReadLine();
+        int zmNumInt = int.Parse(zmNumString);
+        return zmNumInt;
+    }
+    static double SquareNumber(int zmNumber)
+    {
+        double zmNumSquared = zmNumber * zmNumber;
+        return zmNumSquared;
+    }
+    
+    static void DisplayResult(string zmName, double zmSquaredNumber);
+    {
+        Console.WriteLine($"{zmName}, the square of your number is {zmSquaredNumber}");
     }
 }
