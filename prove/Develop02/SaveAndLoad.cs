691916670f259838307f5c419c1587c71c1bac13
Zachwm <zachwm@byui.edu>
2023-10-06 22:27:07 -0600

    Extra done

--- prove/Develop02/SaveAndLoad.cs
+++ prove/Develop02/SaveAndLoad.cs
@@ -5,13 +5,14 @@ class SaveAndLoad
     public List<Entry> Load() {
         Console.WriteLine("What is the file name? ");
         string userFileName = Console.ReadLine();
+        userFileName = userFileName + ".csv";
 
         string[] _lines = System.IO.File.ReadAllLines(userFileName);
         List<Entry> loadedEntries = new List<Entry> {};  
         foreach(string line in _lines)
         {
             if (line.Length > 0) {
-                string[] entryAttributes = line.Split("~");
+                string[] entryAttributes = line.Split(",");
                 Entry newEntry = new Entry();
                 newEntry.date = entryAttributes[0];
                 newEntry.prompt = entryAttributes[1];
@@ -26,10 +27,14 @@ class SaveAndLoad
 
     public void Save(List<Entry> entriesToSave) { 
         Console.WriteLine("What is the file name? ");
-        string userFileName = Console.ReadLine(); 
+        string userFileName = Console.ReadLine();
+        userFileName = userFileName + ".csv";
         string entriesToSaveString = "";
-        foreach(Entry entryToSave in entriesToSave) {
-            entriesToSaveString += $"{entryToSave.date}~{entryToSave.prompt}~{entryToSave.response}\n";
+        foreach (Entry entryToSave in entriesToSave) 
+        {   
+            string response = $"\"{entryToSave.response.Replace("\"", "\"\"")}\"";
+
+            entriesToSaveString += $"{entryToSave.date},{entryToSave.prompt},{response}\n";
         }
 
         using (StreamWriter outputFile = new StreamWriter(userFileName))
@@ -38,6 +43,4 @@ class SaveAndLoad
         outputFile.WriteLine(entriesToSaveString);
     
     }
-
-    
 }
