2081024e1c5a8f2f03e3797a1475d0355e9498a9
Zachwm <zachwm@byui.edu>
2023-10-06 22:36:23 -0600

    Comment added

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,6 +2,7 @@
 // CSE 210
 // Zach McLaughlin
 // An application that promts the user for a journal entry allowing the entry to be saved to a file and later loaded
+// I completed exceeding expectations by correctly saving the journal into a csv file taking into account the possibility of any commas in their input
 
 using System;
 
